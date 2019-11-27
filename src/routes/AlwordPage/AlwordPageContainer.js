a328065a77ecc6f58c062756dfa474749ecaa68a
Alword <anzer987@yandex.ru>
2019-11-27 22:47:22 +0300

    feature fullscreen canvas

--- src/routes/AlwordPage/AlwordPageContainer.js
+++ src/routes/AlwordPage/AlwordPageContainer.js
@@ -5,16 +5,34 @@ function AlwordPageContainer() {
   const [data, setData] = React.useState([]);
 
   React.useEffect(() => {
-    fetch('https://www.cbr-xml-daily.ru/daily_json.js')
-      .then(response => response.json())
-      .then(data => {
-        setData(
-            Object.values(data.Valute).map(({ CharCode, Value }) => ({
-            name: CharCode,
-            value: Value
-          }))
-        );
-      });
+    
+    var canvasElement = document.querySelector("#canvas");
+    var ctx = canvasElement.getContext("2d");
+    
+    // Sides: a = 30   b = 30   c = 59
+
+    canvasElement.width = window.innerWidth;
+    canvasElement.height = window.innerHeight;
+
+    var triangle = {
+      x1: 30,
+      y1: 0,
+      x2: 0,
+      y2: 59,
+      x3: 30,
+      y3: 59
+    }
+
+    ctx.strokeStyle = 'red';
+
+    ctx.beginPath();
+    ctx.moveTo(triangle.x1, triangle.y1);
+    ctx.lineTo(triangle.x2, triangle.y2);
+    ctx.lineTo(triangle.x3, triangle.y3);
+    ctx.lineTo(triangle.x1, triangle.y1);
+    ctx.closePath();
+    ctx.stroke();
+
   }, []);
 
   return <AlwordPage data={data} />;
