e0e134493ef409fe7e7552c3ec980c786f0514ca
Alword <anzer987@yandex.ru>
2019-12-20 23:35:35 +0300

    two color palette

--- src/routes/AlwordPage/AlwordPage.js
+++ src/routes/AlwordPage/AlwordPage.js
@@ -2,21 +2,43 @@ import React from 'react';
 import AppBar from 'src/components/AppBar';
 import { useRef } from 'react'
 import { useEffect } from 'react'
-
+import { SketchPicker } from 'react-color'
 
 import './Alword-page.css';
 
+
 function AlwordPage() {
   const ref = useRef(null);
+  const [data, setData] = React.useState({ background: '#fff', foreground: "#ff0000" });
+  const handleBackGround = (color) => {
+    setData({ background: color.hex });
+  };
+  const handleForeground = (color) => {
+    setData({ foreground: color.hex });
+  };
+
   useEffect(() => {
-    if (ref.current) {
+    console.table(ref.current);
+    if (ref.current && ref.current.children.length == 0) {
       Process(ref);
     }
   });
+
   return (
     <>
-      <AppBar title="Слепушко" color="#000" />
-      <div ref={ref} />
+      <AppBar title="Слепушко" />
+      <div ref={ref} class="canvas" style={{ background: data.background }} />
+      <div style={{float:"left"}}>
+      <SketchPicker
+        color={data.background}
+        onChangeComplete={handleBackGround} />
+      </div>
+      
+      <div style={{float:"right"}}>
+        <SketchPicker
+          color={data.foreground}
+          onChangeComplete={handleForeground} />
+      </div>
     </>
   );
 }
@@ -26,7 +48,7 @@ export default AlwordPage;
 
 var SPEED = 100;
 function Process(ref) {
-  
+
   var FPS = 60;
 
   if (!window.requestAnimationFrame) {
@@ -49,19 +71,19 @@ function Process(ref) {
   var canvas, context;
   //var height = window.screen.height-164;
   console.table(document.documentElement);
-  var height = document.documentElement.clientHeight-64;
+  var height = document.documentElement.clientHeight - 64;
   var width = document.documentElement.clientWidth;
   var dots = [{
     x: 0,
     y: height
   }, {
-    x: width/2,
+    x: width / 2,
     y: 0
   }, {
     x: width,
     y: height
   }];
-  var currentDot = {x:dots[0].x,y:dots[0].y}
+  var currentDot = { x: dots[0].x, y: dots[0].y }
   init();
   animate();
 
@@ -72,7 +94,7 @@ function Process(ref) {
     canvas.height = height;
     context = canvas.getContext('2d');
     context.fillStyle = 'rgb(0,0,0)';
-    context.fillRect(0, 0, width, height);
+    //context.fillRect(0, 0, width, height);
     ref.current.appendChild(canvas);
   }
 
@@ -80,7 +102,7 @@ function Process(ref) {
 
     requestAnimationFrame(animate);
 
-    for(let i = 0; i<SPEED; i++){
+    for (let i = 0; i < SPEED; i++) {
       draw();
     }
 
@@ -90,7 +112,7 @@ function Process(ref) {
 
     var time = new Date().getTime() * 0.001;
     console.log(dots.length);
-    let dot = dots[getRandomInt(0, dots.length-1)];
+    let dot = dots[getRandomInt(0, dots.length - 1)];
 
     var x = currentDot.x + (dot.x - currentDot.x) / 2;
     var y = currentDot.y + (dot.y - currentDot.y) / 2;
