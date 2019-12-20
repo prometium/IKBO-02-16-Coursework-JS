afcab86a2e36985826d1416136efb017f26b02f9
Alword <anzer987@yandex.ru>
2019-12-21 00:31:38 +0300

    color change

--- src/routes/AlwordPage/AlwordPage.js
+++ src/routes/AlwordPage/AlwordPage.js
@@ -1,3 +1,4 @@
+//@ts-check
 import React from 'react';
 import AppBar from 'src/components/AppBar';
 import { useRef } from 'react'
@@ -7,18 +8,120 @@ import { SketchPicker } from 'react-color'
 import './Alword-page.css';
 
 
+
+
 function AlwordPage() {
   const ref = useRef(null);
   const [data, setData] = React.useState({ background: '#fff', foreground: "#ff0000" });
+  
+  let FPS = 1; 
+  let SPEED = 100;
+  let height = document.documentElement.clientHeight - 64;
+  let width = document.documentElement.clientWidth;
+  let foreground = data.foreground;
+  let background = data.background;
+  let updateBackground = false;
   const handleBackGround = (color) => {
-    setData({ background: color.hex });
+    background = color.hex;
+    setData({ background: background, foreground: foreground });
+    updateBackground = true;
   };
   const handleForeground = (color) => {
-    setData({ foreground: color.hex });
+    foreground = color.hex;
+    setData({ background: background, foreground: foreground });
+    updateBackground = true;
   };
 
+  function Process(ref) {
+
+    if (!window.requestAnimationFrame) {
+
+      window.requestAnimationFrame = (function () {
+
+        return window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame ||
+          window.oRequestAnimationFrame ||
+          window.msRequestAnimationFrame ||
+          function ( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+            window.setTimeout(callback, 1000 / FPS);
+          };
+
+      })();
+    }
+
+    let canvas, context;
+    //var height = window.screen.height-164;
+    console.table(document.documentElement);
+    let dots = [{
+      x: 0,
+      y: height
+    }, {
+      x: width / 2,
+      y: 0
+    }, {
+      x: width,
+      y: height
+    }];
+    let currentDot = { x: dots[0].x, y: dots[0].y }
+    init();
+    animate();
+
+    function init() {
+      
+      canvas = document.createElement('canvas');
+      canvas.width = width;
+      canvas.height = height;
+      context = canvas.getContext('2d');
+      context.fillStyle = background;
+      context.fillRect(0,0,width,height);
+      ref.current.appendChild(canvas);
+    }
+
+    function animate() {
+
+      requestAnimationFrame(animate);
+
+      for (let i = 0; i < SPEED; i++) {
+        draw();
+      }
+
+    }
+    
+    function draw() {
+
+      if(updateBackground)
+      {
+        context.fillStyle = background;
+        context.fillRect(0, 0, width, height);
+        updateBackground = false;
+      }
+
+      let dot = dots[getRandomInt(0, dots.length - 1)];
+
+      currentDot.x += (dot.x - currentDot.x) / 2;
+      currentDot.y += (dot.y - currentDot.y) / 2;
+
+      context.fillStyle = foreground;
+      context.beginPath();
+      context.arc(currentDot.x, currentDot.y, 1, 0, Math.PI * 2, true);
+      context.closePath();
+      context.fill();
+    }
+
+    // использование Math.round() даст неравномерное распределение!
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+  }
+
   useEffect(() => {
-    console.table(ref.current);
+    console.log("Effect")
+
+    while (ref.current.firstChild) {
+      ref.current.removeChild(ref.current.firstChild);
+    }
+
     if (ref.current && ref.current.children.length == 0) {
       Process(ref);
     }
@@ -27,14 +130,14 @@ function AlwordPage() {
   return (
     <>
       <AppBar title="Слепушко" />
-      <div ref={ref} class="canvas" style={{ background: data.background }} />
-      <div style={{float:"left"}}>
-      <SketchPicker
-        color={data.background}
-        onChangeComplete={handleBackGround} />
+      <div ref={ref} className="canvas"/>
+      <div style={{ float: "left" }}>
+        <SketchPicker
+          color={data.background}
+          onChangeComplete={handleBackGround} />
       </div>
-      
-      <div style={{float:"right"}}>
+
+      <div style={{ float: "right" }}>
         <SketchPicker
           color={data.foreground}
           onChangeComplete={handleForeground} />
@@ -43,95 +146,5 @@ function AlwordPage() {
   );
 }
 
+
 export default AlwordPage;
-
-
-var SPEED = 100;
-function Process(ref) {
-
-  var FPS = 60;
-
-  if (!window.requestAnimationFrame) {
-
-    window.requestAnimationFrame = (function () {
-
-      return window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        function ( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
-
-          window.setTimeout(callback, 1000 / FPS);
-
-        };
-
-    })();
-  }
-
-  var canvas, context;
-  //var height = window.screen.height-164;
-  console.table(document.documentElement);
-  var height = document.documentElement.clientHeight - 64;
-  var width = document.documentElement.clientWidth;
-  var dots = [{
-    x: 0,
-    y: height
-  }, {
-    x: width / 2,
-    y: 0
-  }, {
-    x: width,
-    y: height
-  }];
-  var currentDot = { x: dots[0].x, y: dots[0].y }
-  init();
-  animate();
-
-  function init() {
-
-    canvas = document.createElement('canvas');
-    canvas.width = width;
-    canvas.height = height;
-    context = canvas.getContext('2d');
-    context.fillStyle = 'rgb(0,0,0)';
-    //context.fillRect(0, 0, width, height);
-    ref.current.appendChild(canvas);
-  }
-
-  function animate() {
-
-    requestAnimationFrame(animate);
-
-    for (let i = 0; i < SPEED; i++) {
-      draw();
-    }
-
-  }
-
-  function draw() {
-
-    var time = new Date().getTime() * 0.001;
-    console.log(dots.length);
-    let dot = dots[getRandomInt(0, dots.length - 1)];
-
-    var x = currentDot.x + (dot.x - currentDot.x) / 2;
-    var y = currentDot.y + (dot.y - currentDot.y) / 2;
-    currentDot.x = x;
-    currentDot.y = y;
-    //context.fillStyle = 'rgb(245,245,245)';
-    //context.fillRect( 0, 0, 255, 255 );
-
-    context.fillStyle = 'rgb(255,0,0)';
-    context.beginPath();
-    context.arc(x, y, 1, 0, Math.PI * 2, true);
-    context.closePath();
-    context.fill();
-
-  }
-
-  // использование Math.round() даст неравномерное распределение!
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-}
