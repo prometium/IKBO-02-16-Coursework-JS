e748eb82abcd240bca8cd8b3f9980c4ab86e4748
Wo1kowsky <43152077+Wo1kowsky@users.noreply.github.com>
2019-12-21 19:56:35 +0300

    setting meme url via state

--- src/routes/Wo1kowskyPage/Wo1kowskyPage.js
+++ src/routes/Wo1kowskyPage/Wo1kowskyPage.js
@@ -5,42 +5,62 @@ import Button from "../../components/Button";
 import fetchJsonp from "fetch-jsonp";
 
 
-function next_mem(memes, i) {
-    let isSingle = false;
-    while (!isSingle) {
-        if (memes[i]['attachments'].length === 1) {
-            isSingle = true;
-            return i;
+function max_meme(memes) {
+    let i = 0;
+    let max = 0;
+    let max_mem;
+    while (i < memes.length) {
+        if (memes[i]['attachments'] && memes[i]['attachments'].length === 1) {
+            if (memes[i]['likes']['count'] > max) {
+                max = memes[i]['likes']['count'];
+                max_mem = memes[i];
+            }
         }
-        console.log(i);
         i += 1;
     }
+    return max_mem;
 }
 
 
 function log_json(json) {
+    //console.log(json);
     let memes = json['response']['items'];
-    let mem = memes[next_mem(memes, 0)];
+    let mem = max_meme(memes);
     let tmp = mem['attachments'][0]['photo']['sizes'];
-    console.log(tmp[tmp.length-1]['url']);
+    return tmp[tmp.length-1]['url'];
 }
+
+const api = () =>
+    fetchJsonp('https://api.vk.com/method/wall.get?access_token=abcabc1aabcabc1aabcabc1a39aba40940aabcaabcabc1af63747417444a9b169b5cad1&v=5.92&domain=ru9gag&count=10&offset=0')
+        .then(response => response.json())
+        .then(function (json) {
+            return  log_json(json)
+        });
 
 
 function Wo1kowskyPage() {
+    const [url, setUrl] = React.useState('https://img1.looper.com/img/gallery/things-only-adults-notice-in-shrek/intro-1573597941.jpg');
 
-    let url = 'https://api.vk.com/method/wall.get?access_token=abcabc1aabcabc1aabcabc1a39aba40940aabcaabcabc1af63747417444a9b169b5cad1&v=5.92&domain=ru9gag&count=2&offset=0';
+    React.useEffect(() => {
+        api().then(meme_url =>{
+            setUrl(meme_url)
+        })
+    }, []);
+
+    /*let url = 'https://api.vk.com/method/wall.get?access_token=abcabc1aabcabc1aabcabc1a39aba40940aabcaabcabc1af63747417444a9b169b5cad1&v=5.92&domain=ru9gag&count=10&offset=0';
     fetchJsonp(url)
         .then(response => response.json())
         .then(function (json) {
             log_json(json)
         });
+        */
 
     return (
         <>
             <AppBar title="Волков" color='#ff69b4'/>
             <Button/>
             <AutoFitImage frameWidth="500px" frameHeight="400px"
-                          imgSrc="https://img1.looper.com/img/gallery/things-only-adults-notice-in-shrek/intro-1573597941.jpg"
+                          imgSrc={url}
                           style={{/*..your style here..*/}}/>
 
         </>
