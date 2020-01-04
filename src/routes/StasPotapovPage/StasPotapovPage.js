32fd0e4bd476c4d07070a525d95672f6ad6ed4bc
StasPotapov <kapitanoc@yandex.ru>
2020-01-04 21:06:13 +0300

    use spinner in my page

--- src/routes/StasPotapovPage/StasPotapovPage.js
+++ src/routes/StasPotapovPage/StasPotapovPage.js
@@ -9,47 +9,35 @@ import './StasPotapovPage.css';
 function StasPotapovPage() {
   const [openX, setOpenX] = React.useState();
   const [openY, setOpenY] = React.useState();
+  const [run, setRun] = React.useState(false);
 
   const handleMouseDownX = () => {
+    setRun(true);
     setOpenX(!openX);
   };
 
-  const handleTouchStartX = e => {
-    e.preventDefault();
-    handleMouseDownX();
-  };
-
   const handleMouseDownY = () => {
+    setRun(true);
     setOpenY(!openY);
-  };
-
-  const handleTouchStartY = e => {
-    e.preventDefault();
-    handleMouseDownY();
   };
 
   return (
     <>
       <AppBar title="Потапов" color="#1565c0" />
       <div className="potapov">
-        <div>
-          <Spinner show={true} />
-          <Button
-            onMouseDown={handleMouseDownX}
-            onTouchStart={handleTouchStartX}
-            primary
-          >
+        <Spinner show={run} />
+        <div className="potapov-buttons">
+          <Button onMouseDown={handleMouseDownX} primary>
             {openX ? 'Влево' : 'Вправо'}
           </Button>
-          <Button
-            onMouseDown={handleMouseDownY}
-            onTouchStart={handleTouchStartY}
-            primary
-          >
+          <Button onMouseDown={handleMouseDownY} primary>
             {openY ? 'Вверх' : 'Вниз'}
           </Button>
         </div>
         <Motion
+          onRest={() => {
+            setRun(false);
+          }}
           style={{ x: spring(openX ? 400 : 0), y: spring(openY ? 400 : 0) }}
         >
           {({ x, y }) => (
